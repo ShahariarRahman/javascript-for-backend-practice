911078615b6b62d802580d03e1ac3dc797dcc3ce
ShahariarRahman <shahariarrahman98@gmail.com>
2023-03-06 03:36:26 +0600

    Global errorHandler function

--- 4. Errors and Error Handling/errors.js
+++ 4. Errors and Error Handling/errors.js
@@ -2,10 +2,14 @@ const getData = async () => {
   try {
     undefined.find();
   } catch (error) {
-    //
+    errorHandler(error);
   }
 };
 
 getData();
+
+const errorHandler = (error) => {
+  console.log(error);
+};
 
 console.log("Done!");
